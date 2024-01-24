5ea795c1bbd297e6be7bcfe5b883ece343a7e3f2
osaroo3 <>
2024-01-24 04:09:55 +0000

    Initial commit

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    // $("h2").addClass("underline"); //add underline class to all <h2> elements
+    // $("ul").addClass("border");  // adds border class to all ul 
+});
